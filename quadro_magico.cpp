2a734aa51ccdb61bb6d9da7c8fae340aada0d948
pedrocanddz <pedrocanddz07@gmail.com>
2023-03-03 19:26:54 -0300

    initial commit

---
+++ quadro_magico.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int matrix[n][n];
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    return 0;
+}
