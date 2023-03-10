fbac6857ac1b1575b6563537b8dd76bdbac2578d
pedrocanddz <pedrocanddz07@gmail.com>
2023-03-10 16:59:19 -0300

    lista 1

--- ilya_and_querries.cpp
+++ ilya_and_querries.cpp
@@ -5,23 +5,33 @@ using namespace std;
 int main()
 {
     string s;
-
-    cin >> s;
+    char c;
+    int soma_max[100000];
+    while (scanf("%c", &c))
+    {
+        if (c == '\n')
+            break;
+        if (s.length() == 0)
+        {
+            soma_max[0] = 1;
+        }
+        else
+        {
+            if (s[s.length() - 1] == c)
+                soma_max[s.length()] = soma_max[s.length() - 1] + 1;
+            else
+                soma_max[s.length()] = soma_max[s.length() - 1];
+        }
+        s += c;
+    }
     int n, l, r;
     cin >> n;
 
     for (int i = 0; i < n; i++)
     {
         cin >> l >> r;
-        int count = 0;
-        for (int j = l - 1; j < r - 1; j++)
-        {
-            if (s[j] == s[j + 1])
-            {
-                count++;
-            }
-        }
-        cout << count << "\n";
+        int soma = soma_max[r - 1] - soma_max[l - 1];
+        cout << soma << "\n";
     }
 
     return 0;
